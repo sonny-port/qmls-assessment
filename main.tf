35c854320e4c3ce49a3335fb36f16cd3cf1ac45a
roselio <roselio.viray@gmail.com>
2023-01-30 21:30:31 +0800

    .

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
 terraform {
-    required_version = ">=1.2"
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.49.0"
+    }
+  }
 }
+
+provider "openstack" {
+}
+
  
 resource "null_resource" "ansible-install" {
     provisioner "local-exec" {
